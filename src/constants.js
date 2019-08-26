a7d6a01baa2d8ad8e6d99bcc931a64efe791344b
asmin <sminmgr@gmail.com>
2019-08-26 22:19:48 +0545

    Format code according to prettier

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
 export const QUERY_NAMES = {
-  MEMBERS_WITH_ROLE: "membersWithRole",
-  FETCH_MORE_MEMBERS: "fetchMoreMembers",
-  FETCH_USER_EVENTS: "fetchUserEvents"
+  MEMBERS_WITH_ROLE: 'membersWithRole',
+  FETCH_MORE_MEMBERS: 'fetchMoreMembers',
+  FETCH_USER_EVENTS: 'fetchUserEvents',
 };
 
 export const DAYS_TO_CONSIDER = 7;
 
 export const eventTypes = {
-  PULL_REQUESTS: "pullRequests",
-  ISSUES: "issues"
+  PULL_REQUESTS: 'pullRequests',
+  ISSUES: 'issues',
 };
 
 export const STATES = {
-  OPEN: "OPEN",
-  CLOSED: "CLOSED"
+  OPEN: 'OPEN',
+  CLOSED: 'CLOSED',
 };
--- src/query.js
+++ src/query.js
@@ -1,28 +1,28 @@
-import { QUERY_NAMES } from "./constants";
+import { QUERY_NAMES } from './constants';
 
 const getQuery = (queryName, variables) => {
   switch (queryName) {
     case QUERY_NAMES.MEMBERS_WITH_ROLE: {
       return {
         variables: {
-          first: 100
+          first: 100,
         },
         query:
-          "query($first: Int!) {organization(login:leapfrogtechnology) {name, url, membersWithRole (first:$first){totalCount, pageInfo {hasNextPage, endCursor}, nodes{login, name}}}}"
+          'query($first: Int!) {organization(login:leapfrogtechnology) {name, url, membersWithRole (first:$first){totalCount, pageInfo {hasNextPage, endCursor}, nodes{login, name}}}}',
       };
     }
     case QUERY_NAMES.FETCH_MORE_MEMBERS: {
       return {
         variables: variables,
         query:
-          "query ($first: Int!, $after: String!) {organization(login:leapfrogtechnology) {name, url, membersWithRole (first:$first, after:$after){totalCount, pageInfo {hasNextPage, endCursor}, nodes{login, name}}}}"
+          'query ($first: Int!, $after: String!) {organization(login:leapfrogtechnology) {name, url, membersWithRole (first:$first, after:$after){totalCount, pageInfo {hasNextPage, endCursor}, nodes{login, name}}}}',
       };
     }
     case QUERY_NAMES.FETCH_USER_EVENTS: {
       return {
         variables: variables,
         query:
-          "query($user:String!,$pullRequestsAfter:String,$issuesAfter:String,$issueState:[IssueState!],$pullRequestState:[PullRequestState!],){user(login:$user){email,pullRequests(first:100,after: $pullRequestsAfter,states:$pullRequestState,orderBy: {field: UPDATED_AT, direction: DESC}){pageInfo {hasNextPage, endCursor},edges{node{title,body,updatedAt}}},issues(first:100,after: $issuesAfter,states:$issueState,orderBy: {field: UPDATED_AT, direction: DESC}){pageInfo {hasNextPage, endCursor},edges{node{title,body,updatedAt}}}}}"
+          'query($user:String!,$pullRequestsAfter:String,$issuesAfter:String,$issueState:[IssueState!],$pullRequestState:[PullRequestState!],){user(login:$user){email,pullRequests(first:100,after: $pullRequestsAfter,states:$pullRequestState,orderBy: {field: UPDATED_AT, direction: DESC}){pageInfo {hasNextPage, endCursor},edges{node{title,body,updatedAt}}},issues(first:100,after: $issuesAfter,states:$issueState,orderBy: {field: UPDATED_AT, direction: DESC}){pageInfo {hasNextPage, endCursor},edges{node{title,body,updatedAt}}}}}',
       };
     }
   }
