59509d389abff4370833ddf05dd0bf1e275794b1
nischalshakya95 <nischalshakya95@gmail.com>
2019-09-13 12:52:23 +0545

    Add comments above method

--- src/service/create-markdown.js
+++ src/service/create-markdown.js
@@ -1,5 +1,16 @@
 import fs from 'fs';
 
+/**
+ * Write data content to external markdown file 
+ * let fileName = users.md
+ * let data = firstname | lastname
+                --- | ---  
+              Nischal | Shakya 
+              Avishkar | KC
+ * 
+ * @param {string} fileName 
+ * @param {object} data 
+*/
 export function createMarkdown(fileName, data) {
   fs.writeFile(fileName, data, err => {
     if (err) {
--- src/util/key-value-util.js
+++ src/util/key-value-util.js
@@ -1,7 +1,25 @@
+/**
+ * Filter keys from flat json
+ *
+ * let json = [{name : "Nischal" lastname: "Shakya"}, {"name": "Avishkar", lastnane: "KC"}]
+ *
+ * @param {json} json
+ * @return ["name", "lastname"]
+ */
 export function getKeys(json) {
   return Object.keys(json[json.length - 1]);
 }
 
+/**
+ * Filter values from json array
+ *
+ * let json = [{name : "Nischal" lastname: "Shakya"}, {"name": "Avishkar", lastnane: "KC"}]
+ * let keys = ["name", "lastname"]
+ *
+ * @param {json} json
+ * @param {string[]} keys
+ * @return [["Nischal", "Shakya"], ["Avishkar", "KC"]]
+ */
 export async function getValues(json, keys) {
   const twoDValuesArray = [];
   let keyIndex = 0;
