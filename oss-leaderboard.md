2622daf352ffa0af6f8dc8e31b27f27e95ddf301
asmin <sminmgr@gmail.com>
2019-09-23 20:12:11 +0545

    Add comments for function in search

--- oss-leaderboard.md
+++ oss-leaderboard.md
@@ -1,7 +1,13 @@
 # oss-leaderboard
 
-| Name | PR Merged | PR Opened | Issue Opened | Issue Comments | Repos Contributed | Score |
-| ---- | --------- | --------- | ------------ | -------------- | ----------------- | ----- |
-
+| Name                 | PR Merged | PR Opened | Issue Opened | Issue Comments | Repos Contributed | Score |
+| -------------------- | --------- | --------- | ------------ | -------------- | ----------------- | ----- |
+| Sanjeev Kumar Pandit | 4         | 0         | 0            | 0              | 7                 | 12    |
+| Kabir Baidhya        | 1         | 1         | 3            | 2              | 19                | 11    |
+| Sagar Acharya        | 1         | 1         | 0            | 0              | 4                 | 6     |
+| Nischal Shakya       | 0         | 1         | 0            | 0              | 2                 | 3     |
+| Bimochan Shrestha    | 0         | 1         | 0            | 0              | 33                | 3     |
+| Sanish Maharjan      | 0         | 0         | 0            | 1              | 3                 | 1     |
+| Sushil Ghimire       | 0         | 0         | 0            | 1              | 6                 | 1     |
 
 > PR = Pull request
--- src/util/eventHelper.js
+++ src/util/eventHelper.js
@@ -1,31 +1,48 @@
 /**
- * Binary search to count total events which lies after target date
+ * Binary search to find least greater index than given date.
  *
  * @param {*} events Array of events.
- * @param {*} l Left bound.
- * @param {*} r Right bound.
+ * @param {*} leftBound Left bound.
+ * @param {*} rightBound Right bound.
  */
-export const findLeastGreater = (eventDetail, l, r, boundDate) => {
-  if (l === r) {
-    return l;
+export const findLeastGreater = (
+  eventDetail,
+  leftBound,
+  rightBound,
+  boundDate,
+) => {
+  if (leftBound === rightBound) {
+    return leftBound;
   }
-  const mid = parseInt(l + (r - l) / 2);
+  const mid = parseInt(leftBound + (rightBound - leftBound) / 2);
   const midEventDate = new Date(eventDetail[mid].node.updatedAt);
   if (midEventDate.getTime() >= boundDate.getTime()) {
-    return findLeastGreater(eventDetail, mid + 1, r, boundDate);
+    return findLeastGreater(eventDetail, mid + 1, rightBound, boundDate);
   }
-  return findLeastGreater(eventDetail, l, mid, boundDate);
+  return findLeastGreater(eventDetail, leftBound, mid, boundDate);
 };
 
-export const findGreatestLesser = (eventDetail, l, r, boundDate) => {
-  if (l === r) {
-    return l;
+/**
+ * Binary search to find greatest lesser index than given date.
+ *
+ * @param {*} events Array of events.
+ * @param {*} leftBound Left bound.
+ * @param {*} rightBound Right bound.
+ */
+export const findGreatestLesser = (
+  eventDetail,
+  leftBound,
+  rightBound,
+  boundDate,
+) => {
+  if (leftBound === rightBound) {
+    return leftBound;
   }
-  const mid = parseInt(l + (r - l) / 2);
+  const mid = parseInt(leftBound + (rightBound - leftBound) / 2);
   const midEventDate = new Date(eventDetail[mid].node.updatedAt);
 
   if (midEventDate.getTime() > boundDate.getTime()) {
-    return findGreatestLesser(eventDetail, mid + 1, r, boundDate);
+    return findGreatestLesser(eventDetail, mid + 1, rightBound, boundDate);
   }
-  return findGreatestLesser(eventDetail, l, mid, boundDate);
+  return findGreatestLesser(eventDetail, leftBound, mid, boundDate);
 };
