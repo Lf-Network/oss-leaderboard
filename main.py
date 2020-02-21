6cadcee97dbbec88b5baa2597cbde953e5162ee7
yankeexe <yankee.exe@gmail.com>
2020-02-21 19:03:34 +0545

    Add logging

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 ''' Main module for the OSS Leaderboard. '''
-from queries import test_query
-from fetch_data import execute_query
+import logging
+from leaderboard.query.query import test_query
+from leaderboard.fetch_data import execute_query
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('main')
 
 
 def main(): 
