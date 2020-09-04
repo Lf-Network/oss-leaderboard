f7a96a91e40d45dbc93d5af07dee522aa42efd39
anchal Adhikari <aanchal.adk@gmail.com>
2020-09-04 15:43:27 +0545

    Add function description

--- leaderboard/utils/data.py
+++ leaderboard/utils/data.py
@@ -17,11 +17,35 @@ def get_date(days: int) -> str:
 
 
 def convert_df_to_markdown(df: pd.DataFrame) -> str:
+    """ Convert dataframe to markdown 
+    Args:
+        df: dataframe object
+
+    Returns:
+        markdown string
+    """
+
     return df.to_markdown()
 
 def convert_df_to_html(df: pd.DataFrame) -> str:
+    """ Convert dataframe to html 
+    Args:
+        df: dataframe object
+
+    Returns:
+        html string
+    """
+
     return df.to_html()
 
 
 def convert_mk_to_html(mk_data: str) -> str:
+    """ Convert markdown to html 
+    Args:
+        mk_data: markdown string
+
+    Returns:
+        html string
+    """
+
     return markdown.markdown(mk_data, extensions=["tables"])
