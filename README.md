6d8991fc706706b7419fc94538e05bf80a054bbb
Ishan Manandhar <ishan02016@gmail.com>
2020-10-06 11:15:00 +0545

    addressed comments and made necessary changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 OSS Leaderboard shows the top contributors based on their contribution activities on GitHub.
 
-<img src = "https://imgur.com/o32dq3s" width = "700">
+<img src = "https://i.imgur.com/o32dq3s.png" width = "700">
 
 ## Usage
 
--- final_html_output.py
+++ final_html_output.py
@@ -10,30 +10,11 @@ def final_html_output(df: pd.DataFrame) -> str:
     Returns:
         html string
     """
-    html_string = """
-  <html>
-    <head>
-      <title>Opensource Leaderboard Leapforg</title>
-    </head>
-    <link rel="stylesheet" type="text/css" href="../assets/style.css" />
-    <body>
-      <div class="leaderboard">
-        <img
-        class="leaderboard__img"
-        src="../assets/LF_Opensource.svg"
-        alt="Leapfrog Opensource Logo"
-        />
-        <h1>Frogtoberfest Leaderboard</h1>
-        <div class="leaderboard__table">
-          {table}
-          </section>
-        </div>
-    </body>
-  </html>
+    with open("./assets/index.html") as file:
+        html_string = file.read()
 
-    """
     if not os.path.exists("build"):
         os.mkdir("build")
+        
     with open("build/index.html", "w") as f:
         f.write(html_string.format(table=df.to_html(index=False)))
-        f.close()
