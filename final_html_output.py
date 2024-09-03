bcb108930ae1b948a472cb0f005717f92bd7e2b7
saahil-mahato <saahilmahato11@gmail.com>
2024-09-03 14:24:15 +0545

    feat: add integration test

--- final_html_output.py
+++ final_html_output.py
@@ -1,12 +1,10 @@
-import os
 import pandas as pd
 
 
 def final_html_output(df: pd.DataFrame) -> str:
     """Get final html output
 
-    This function reads an HTML file, replaces a placeholder with a table generated from a pandas DataFrame,
-    and writes the resulting HTML to a file.
+    This function reads an HTML file, replaces a placeholder with a table generated from a pandas DataFrame.
 
     Args:
         df: A pandas DataFrame containing the data to be displayed in the table.
@@ -17,12 +15,7 @@ def final_html_output(df: pd.DataFrame) -> str:
     with open("./assets/index.html") as file:
         html_string = file.read()
 
-    if not os.path.exists("build"):
-        os.mkdir("build")
-
-    with open("build/index.html", "w") as f:
-        f.write(
-            html_string.format(
+    html_string = html_string.format(
                 table=df.to_html(
                     index=False,
                     escape=False,
@@ -31,7 +24,8 @@ def final_html_output(df: pd.DataFrame) -> str:
                     },
                 )
             )
-        )
+
+    return html_string
 
 
 def format_username(username: str):
